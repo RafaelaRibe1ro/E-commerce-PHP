a158252e6d6f03af27534b8949692afbc75062f1
Rafaela Oliveira Ribeiro <rafaela.o.ribeiro@hotmail.com>
2022-10-21 22:59:17 -0300

    controller - show

--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -13,16 +13,33 @@ class CategoriaController extends Controller
 
     public function index()
     {
+        $categorias = Categoria::all();
+        return view('categoria.index', compact('categorias'));
     }
 
 
     public function create()
     {
+        return view('categoria.crud');
     }
 
 
     public function store(Request $request)
     {
+        $rules = [
+            'categoria' => 'required|string|max:50',
+        ];
+
+        $data = $request->validate($rules);
+        $categorias = Categoria::create($data);
+
+        return redirect()->route('categoria.index')->with('success', 'Categoria cadastrada com sucesso');
+    }
+
+    public function show($id)
+    {
+        $categorias = Categoria::find($id);
+        return response()->json($categorias);
     }
 
     public function edit($id)
