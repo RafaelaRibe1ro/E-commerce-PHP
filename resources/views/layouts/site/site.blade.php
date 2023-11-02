36b303bf37d529440de67de4454238eab55e2569
Rafaela Oliveira Ribeiro <rafaela.o.ribeiro@hotmail.com>
2023-11-02 13:49:32 -0300

    changing title

--- resources/views/layouts/site/site.blade.php
+++ resources/views/layouts/site/site.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>@yield('titulo')</title>
+    <title>RED GAMER</title>
 
     {{-- <link href="{{ asset('material') }}/css/site.css" rel="stylesheet" /> --}}
     <link href="{{ asset('material') }}/css/material-dashboard.css" rel="stylesheet" />
